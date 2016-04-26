2e16ab81ab9cc0c9150dc554ed7c1457c1116785
bix_reviews <eamarelle@bix.solutions>
2016-04-26 19:54:56 -0300

    Finish url handlers

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,35 +1,35 @@
 'use strict';
 
 angular.module('app.core').controller('HeaderCoreController', ['APP_BRAND', '$scope', 'Menus', '$state', 'Auth', 'Urls',
-	function (APP_BRAND, $scope, Menus, $state, Auth, Urls) {
+    function (APP_BRAND, $scope, Menus, $state, Auth, Urls) {
         $scope.brand = APP_BRAND.BIG;
         $scope.brandSmall = APP_BRAND.SMALL;
 
-		$scope.isCollapsed = false;
-		$scope.menu = Menus.getMenu('topbar');
+        $scope.isCollapsed = false;
+        $scope.menu = Menus.getMenu('topbar');
 
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
+        $scope.toggleCollapsibleMenu = function () {
+            $scope.isCollapsed = !$scope.isCollapsed;
+        };
 
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', function() {
-			$scope.isCollapsed = false;
-		});
+        // Collapsing the menu after navigation
+        $scope.$on('$stateChangeSuccess', function () {
+            $scope.isCollapsed = false;
+        });
 
-		$scope.logout = function(){
-        	//$windo	w.keycloakAuth.logout();
-        	Auth.logout();
-        	$state.go('home.main');
-    	};
+        $scope.logout = function () {
+            //$windo	w.keycloakAuth.logout();
+            Auth.logout();
+            $state.go('home.main');
+        };
 
-		function init(){
-			Urls.getUrls().then(function(res){
-				$scope.urls =res;
-			});
-		}
+        // Load database url
+        function init() {
+            Urls.getUrls().then(function (res) {
+                $scope.urls = res;
+            });
+        }
 
-		init();
-
-	}
+        init();
+    }
 ]);
