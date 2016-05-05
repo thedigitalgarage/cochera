1ad31b987bf0a3ebb48df5d451b96e80714858ca
bix_reviews <eamarelle@bix.solutions>
2016-05-05 14:01:30 -0300

    Add url models and profile

---
+++ server/datasources.development.js
@@ -0,0 +1,19 @@
+var MONGO_HOST = process.env.MONGODB_SERVICE_HOST || 'localhost';
+var MONGODB_DATABASE = process.env.MONGODB_DATABASE || 'cochera';
+var MONGODB_USER = process.env.MONGODB_USER || 'cochera';
+var MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'cochera';
+
+module.exports = {
+    db: {
+        name: "db",
+        connector: "memory"
+    },
+    mongo: {
+        name: "mongo",
+        connector: "mongodb",
+        host: MONGO_HOST,
+        database: MONGODB_DATABASE,
+        //user: MONGODB_USER,
+        //password: MONGODB_PASSWORD
+    }
+};
